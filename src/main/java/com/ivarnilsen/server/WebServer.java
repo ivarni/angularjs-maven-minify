92faacc4ac9b03210ed35eddd13d5ceaa9e854e3
Ivar Nilsen <post@ivarnilsen.com>
2013-05-08 18:04:38 +0200

    Change Jetty port

--- src/main/java/com/ivarnilsen/server/WebServer.java
+++ src/main/java/com/ivarnilsen/server/WebServer.java
@@ -7,7 +7,7 @@ public class WebServer {
 
 	public static void main(String[] args) throws Exception {
 
-		Server server = new Server(8080);
+		Server server = new Server(8088);
 		server.addHandler(new WebAppContext("src/main/webapp", "/"));
 		server.start();
 	}
